bc9240dadee9dafa8a414d9b6ce71a19b5293dfe
Panthulumahitha <panthulumahitha0620@gmail.com>
2024-12-28 20:58:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+// Select the body element
+const body = document.querySelector("body");
+
+// Create and add the h1 element
+const h1 = document.createElement("h1");
+h1.textContent = "Hello World";
+body.appendChild(h1);
+
+// Create and add the h2 element
+const h2 = document.createElement("h2");
+h2.textContent = "Introducing the Accio Ipsum";
+body.appendChild(h2);
+
+// Create and add the p element
+const p = document.createElement("p");
+p.textContent = "Accio ipsum dolor sit amet, join course, solve assignments, get placed above 5 lpa and live life king size.";
+body.appendChild(p);
